a6f46e262f3a6d445000a4de2193ab1c61c8ec5a
Eva <eua_92@hotmail.com>
2021-06-25 17:51:23 +0300

    Create Employee Ledger Transaction classes

---
+++ FuelStationManagementSystem/FuelStationManagementSystem/Impl/Employee.cs
@@ -0,0 +1,21 @@
+﻿using FuelStationManagementSystem.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStationManagementSystem.Impl
+{
+    public class Employee : Person
+    {
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee() :  base()
+        {
+            DateStart = DateTime.Now;    
+        }
+    }
+}
---
+++ FuelStationManagementSystem/FuelStationManagementSystem/Impl/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using FuelStationManagementSystem.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStationManagementSystem.Impl
+{
+    public class Transaction : Entity
+    {
+        public DateTime Date { get; set; }
+        public Guid CustomerID { get; set; }
+        public decimal DiscountValue { get; set; }
+        public decimal TotalValue { get; set; }
+
+        public Transaction() : base()
+        {
+            Date = DateTime.Now;
+        }
+    }
+}
