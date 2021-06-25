325adbdba86c176e3873bee135b07e2a1394d06d
DoumSof99 <71881384+DoumSof99@users.noreply.github.com>
2021-06-25 17:52:02 +0300

    Feat: Create TransactionLine Class

---
+++ FuelStationManagementSystem/FuelStationManagementSystem/Impl/TransactionLine.cs
@@ -0,0 +1,22 @@
+﻿using FuelStationManagementSystem.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStationManagementSystem.Impl {
+    public class TransactionLine : Entity {
+
+        public Guid TransactionID { get; set; }
+        public Guid ItemID { get; set; }
+        public int Quantity { get; set; }
+        public decimal ItemPrice { get; set; }
+        public decimal Value { get; set; }
+
+        public TransactionLine() {
+
+        }
+
+    }
+}
