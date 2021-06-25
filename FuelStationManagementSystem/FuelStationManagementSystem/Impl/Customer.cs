9bd910c697e457101fce65294c158d79dad4434c
DoumSof99 <71881384+DoumSof99@users.noreply.github.com>
2021-06-25 17:23:08 +0300

    Feat: Create Customer Class

---
+++ FuelStationManagementSystem/FuelStationManagementSystem/Impl/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStationManagementSystem.Impl {
+    public class Customer {
+
+        public int CardNumber { get; set; }
+
+        public Customer() : base() {
+
+        }
+
+    }
+}
